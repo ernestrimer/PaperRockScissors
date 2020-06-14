eb6639b831b993922b7a06fe7d79a3dd47d1db5b
Ernie Rimer <ernest.rimer@icloud.com>
2020-06-14 16:13:37 -0600

    basic html and js logic

--- prs.js
+++ prs.js
@@ -1,160 +1,70 @@
-// Basic Objectives:
-// A user selects rock, paper, or scissors
-print image of a rock, paper and scissors.  User would select one.
-The selection is stored in memory.a
-
-// Computer randomly chooses
-// hint: have your choices in an array and randomly choose 
-Array includes paper, rock or scissors.a
-Randomly select an option from that Array.
-FANCY: show the image.
-
-// Display the user and computers choice on the screen
-// Display the winner
-
-  // Bonus Objectives:
-  // Keep track of wins, losses, & ties  
-  user win variable = 0
-  computer win variable = 0
-
-Score Board: 0 - 0
+//// Basic Objectives:
+//COMPLETE // A user selects rock, paper, or scissors
+//COMPLETE // Computer randomly chooses
+//COMPLETE // hint: have your choices in an array and randomly choose 
+//COMPLETE // Display the user and computers choice on the screen
+//COMPLETE // Display the winner
 
 
-//Working logic
-//Will need to change console.log to return in winLose when using DOM with html
-//Look in slack from Harlan, about how to make the code more DRY
-
-var userChoice = prompt("Choose rock, paper, scissors")
-
-var prs = ["Paper", "Rock", "Scissors"]
-
-var comp = prs[Math.floor(Math.random() * prs.length)];
-
-console.log(userChoice)
-console.log(comp)
+//// Bonus Objectives:
+// Keep track of wins, losses, & ties
+// remember this is only going to be until the user refreshes the browser. We don't have anything fetching from or posting to a database yet!
+//COMPLETE // Use images to make a choice // hint: you can listen for the click event on any HTML element!
+// Be able to start a new game without refreshing
+// Track the percentage of times the user has won/ lost/ tied
+// See if you can add ES 6 to your project!
+// Color a win differently than a loss so it is easy to tell if the user won.
+// hint: add and remove classes: http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript(
 
 
-function winLose()
+document.getElementById("rock").onclick = playRock;
+document.getElementById("paper").onclick = playPaper;
+document.getElementById("scissors").onclick = playScissors;
+  
+  //associated function:
+  function playRock() {
+    play("rock");
+  }
+  
+  //associated function:
+  function playPaper() {
+    play("paper");
+  }
+  
+  //associated function:
+  function playScissors() {
+    play("scissors");
+  }
+
+function play(userChoice)
 {
-  if (userChoice === "Paper")
-  {
-    if(comp == "Paper") {
-      console.log("It's a tie");
-    }
-    else if (comp === "Rock") {
-      console.log("You Win!");
-      //Add 1 point to user win tally
-    }
-    else {
-      console.log("You lose!");
-      //Add 1 point to computer win tally
-    }  
-  }
-
-   else if (userChoice == "Rock") 
-  {
-    if(comp == "Rock") {
-      console.log("It's a tie");
-    }
-    else if (comp == "Scissors") {
-      console.log("You Win!");
-      //Add 1 point to user win tally
-    }
-    else {
-      console.log("You lose!");
-      //Add 1 point to computer win tally
-    }  
-  }
+  compChoice = getCompChoice();
   
-  else if (userChoice == "Scissors") 
-  {
-    if (comp == "Scissors") {
-      console.log("It's a tie");
-    }
-    else if (comp == "Paper") {
-      console.log("You Win!");
-      //Add 1 point to user win tally
-    }
-    else {
-      console.log("You lose!");
-      //Add 1 point to computer win tally
-    }  
-  }
+  //Indicate what the user selected
+  document.getElementById("user").innerHTML = "<p>You played " + userChoice + ".</p>";
+  //Indicate what the computer selected
+  document.getElementById("computer").innerHTML = "<p>The computer played " + compChoice + ".</p>";
   
-  else {
-    console.log("Wrong Input");
+  //Produce DRY code by indicating a tie
+  if (userChoice === compChoice)
+  {
+    document.getElementById("outcome").innerHTML = "<p>Draw. Play Again!</p>";
+  }
+  else if (userChoice === "paper" && compChoice === "rock" || userChoice === "rock" && compChoice === "scissors" || userChoice === "scissors" && compChoice === "paper")
+  {
+    document.getElementById("outcome").innerHTML = "<p>You Win!</p>";
+    //Add 1 point to user score
+  }
+  else
+  {
+    document.getElementById("outcome").innerHTML = "<p>You Lose!</p>";
+    //Add q point to computer score
   }
 }
 
-winLose()
-
-// var userChoice = prompt("Choose Rock, Paper, Scissors")
-
-// var prs = ['Paper', 'Rock', 'Scissors']
-// var comp = prs[Math.floor(Math.random() * prs.length)];
-// console.log(comp)
-
-// function winLose(a, b)
-// {
-//   if (userChoice == "Paper")
-//   {
-//     if(comp == "Paper") {
-//       return "It's a tie";
-//     }
-//     else if (comp == "Rock") {
-//       return "You Win!";
-//       //Add 1 point to user win tally
-//     }
-//     else {
-//       return "You lose!";
-//       //Add 1 point to computer win tally
-//     }  
-//   }
-
-//   else if (userChoice == "Rock") 
-//   {
-//     if(comp == "Rock") {
-//       return "It's a tie";
-//     }
-//     else if (comp == "Scissors") {
-//       return "You Win!";
-//       //Add 1 point to user win tally
-//     }
-//     else {
-//       return "You lose!";
-//       //Add 1 point to computer win tally
-//     }  
-//   }
-  
-//   else if (userChoice == "Scissors") 
-//   {
-//     if (comp == "Scissors") {
-//       return "It's a tie";
-//     }
-//     else if (comp == "Paper") {
-//       return "You Win!";
-//       //Add 1 point to user win tally
-//     }
-//     else {
-//       return "You lose!";
-//       //Add 1 point to computer win tally
-//     }  
-//   }
-  
-//   else {
-//     return "Wrong Input"
-//   }
-// }
-
-// winLose()
-
-
-// remember this is only going to be until the user refreshes the browser. We don't have anything fetching from or posting to a database yet!
-// Use images to make a choice
-// hint: you can listen for the click event on any HTML element!
-// Be able to start a new game without refreshing
-// Track the percentage of times the user has won/ lost/ tied
-
-// See if you can add ES 6 to your project!
-// Color a win differently than a loss so it is easy to tell if the user won.
-// hint: add and remove classes: http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
+function getCompChoice ()
+{
+  var prs = ["paper", "rock", "scissors"];
+  var comp = prs[Math.floor(Math.random() * prs.length)];
+  return comp;
+}
